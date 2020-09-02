9109039c86e5b2136b128044ff709bd87486ea14
lgopedro <68169782+lgopedro@users.noreply.github.com>
2020-09-02 12:36:51 -0400

    cambios en la vista de productos y la barra de busqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdministradorComponent } from './componentes/administrador/administrador.component';
 import { ProductosComponent } from './componentes/productos/productos.component';
 import { VendedorComponent } from './componentes/vendedor/vendedor.component';
-import { LoginUsuarioComponent } from './componentes/login-usuario/login-usuario.component'
-import { LoginAdminComponent} from './componentes/login-admin/login-admin.component';
-
+import { LoginUsuarioComponent } from './componentes/login-usuario/login-usuario.component';
+import { LoginAdminComponent } from './componentes/login-admin/login-admin.component';
+import { AdministradoresComponent } from './componentes/administradores/administradores.component';
 
 const routes: Routes = [
-  {path: "", component:AdministradorComponent},
-  {path: "productos", component:ProductosComponent},
-  {path:  "vendedores", component:VendedorComponent},
-  {path: "login-usuario", component:LoginUsuarioComponent},
-  {path: "login-admin", component:LoginAdminComponent}
+  { path: '', component: AdministradorComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'vendedores', component: VendedorComponent },
+  { path: 'administradores', component: AdministradoresComponent },
+  { path: 'login-usuario', component: LoginUsuarioComponent },
+  { path: 'login-admin', component: LoginAdminComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

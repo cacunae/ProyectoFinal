9f9ebd09ef00f63e4eb2f4e141378e4934ea79c0
lgopedro <68169782+lgopedro@users.noreply.github.com>
2020-09-16 02:59:56 -0300

    Mejora de la tabla busqueda en la vista de ventas, creacion de servicio stock que vincula la base de datos

--- src/app/VistaVendedores/ventas/ventas.component.ts
+++ src/app/VistaVendedores/ventas/ventas.component.ts
@@ -3,6 +3,7 @@ import { DataSource } from '@angular/cdk/table';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Productos } from './../../Models/producto.model';
+import { ProductosService } from 'src/app/services/productos.service';
 
 export interface enSeleccion {
   id: number;
@@ -11,65 +12,46 @@ export interface enSeleccion {
   cantidad: number;
 }
 
-const stock: Productos[] = [
-  {
-    id: 1,
-    nombre: 'Galaxy S9',
-    marca: 'Samsung',
-    precio: 450000,
-    stock: 86,
-    minimo: 15,
-    categoria: 'Telefonia',
-  },
-  {
-    id: 2,
-    nombre: 'P20 lite',
-    marca: 'Huawei',
-    precio: 520000,
-    stock: 120,
-    minimo: 20,
-    categoria: 'Telefonia',
-  },
-  {
-    id: 3,
-    nombre: 'Macbook pro',
-    marca: 'Apple',
-    precio: 30000000,
-    stock: 12,
-    minimo: 10,
-    categoria: 'Telefonia',
-  },
-  {
-    id: 2012,
-    nombre: 'Ipad',
-    marca: 'Apple',
-    precio: 12000000,
-    stock: 7,
-    minimo: 12,
-    categoria: 'Tablets',
-  },
-];
-
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
   styleUrls: ['./ventas.component.css'],
 })
 export class VentasComponent implements OnInit {
-  constructor(private changeDetectorRefs: ChangeDetectorRef) {}
+  constructor(private service: ProductosService) {}
 
-  columnasBusqueda: string[] = ['id', 'nombre', 'precio', 'stock', 'agregar'];
-  dataSource = new MatTableDataSource(stock);
+  ngOnInit(): void {
+    console.log('Obteniendo stock');
+    this.obtenerStock().then((productos) => {
+      console.log('productos obtenidos', productos);
+      this.stock = productos;
+      this.dataSource.data = this.stock;
+      console.log(this.dataSource);
+    });
+  }
+
+  stock: any;
+  dataSource = new MatTableDataSource();
+  columnasBusqueda: string[] = [
+    'id',
+    'nombre',
+    'marca',
+    'precio',
+    'stock',
+    'agregar',
+  ];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   //METODOS Y FUNCIONES DE LA SECCION DE BUSQUEDA DE PRODUCTOS
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
   productoSeleccionado: enSeleccion = {
     id: null,
     nombre: '',
@@ -79,48 +61,38 @@ export class VentasComponent implements OnInit {
 
   seleccionar(producto: Productos) {
     console.log(producto);
-    let i = stock.indexOf(producto);
+    console.log(this.stock);
+    let i = this.stock.indexOf(producto);
     console.log(i);
-    this.productoSeleccionado.id = stock[i].id;
-    this.productoSeleccionado.nombre = stock[i].nombre;
-    this.productoSeleccionado.precio = stock[i].precio;
+    this.productoSeleccionado.id = this.stock[i].id;
+    this.productoSeleccionado.nombre = this.stock[i].nombre;
+    this.productoSeleccionado.precio = this.stock[i].precio;
   }
   agregarProductoSeleccionado() {
     this.productosSeleccionados.push(this.productoSeleccionado);
     console.log(this.productosSeleccionados);
     this.obtenerTotal();
   }
-  // FIN DE BUSQUEDA DE PRODUCTOS
-  ngOnInit(): void {
-    console.log(this.productosSeleccionados);
+  //SERVICIOS
+  obtenerStock() {
+    let promise = new Promise((resolve, reject) => {
+      this.service.obtenerStock().subscribe(
+        (producto) => {
+          resolve(producto);
+        },
+        (error) => {
+          console.log('EXPLOTO ESTO');
+          reject('ERROR AL OBTENER EL STOCK');
+        }
+      );
+      console.log(this.stock);
+    });
+    return promise;
   }
 
-  productosSeleccionados: enSeleccion[] = [
-    {
-      id: 20,
-      nombre: 'Iphone 8x',
-      precio: 1000000,
-      cantidad: 2,
-    },
-    {
-      id: 20,
-      nombre: 'Iphone 8x',
-      precio: 1000000,
-      cantidad: 2,
-    },
-    {
-      id: 20,
-      nombre: 'Iphone 8x',
-      precio: 1000000,
-      cantidad: 2,
-    },
-    {
-      id: 20,
-      nombre: 'Iphone 8x',
-      precio: 1000000,
-      cantidad: 2,
-    },
-  ];
+  // FIN DE BUSQUEDA DE PRODUCTOS
+
+  productosSeleccionados: enSeleccion[] = [];
 
   columnasFactura: string[] = [
     'id',

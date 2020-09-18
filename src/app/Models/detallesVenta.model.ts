a4655033a144900829cab649bd0872e5f4e43fed
lgopedro <68169782+lgopedro@users.noreply.github.com>
2020-09-17 23:31:37 -0300

    YA FUNCIONA EL GUARDAR, bueno, mas o menos :T

---
+++ src/app/Models/detallesVenta.model.ts
@@ -0,0 +1,9 @@
+export interface DetallesVenta {
+  id: number;
+  idVenta: number;
+  idProducto: number;
+  nombreProducto: string;
+  precio: number;
+  cantidad: number;
+  descuento: number;
+}
---
+++ src/app/Models/venta.model.ts
@@ -0,0 +1,7 @@
+export interface Venta {
+  idVenta: number;
+  idVendedor: string;
+  fechaHora: Date;
+  formaDePago: string;
+  total: number;
+}
